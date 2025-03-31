5aa942b906cc2cf94f5658aa834dd223766792f6
henrimichaud21 <hmichaud@unb.ca>
2025-03-31 15:41:19 -0300

    Changed the name from MainActivity to MPABS_GUI

--- MPABS_GUI.py
+++ MPABS_GUI.py
@@ -3,11 +3,11 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import *
 from UI.HomePage import HomePage
 
-class MainActivity(QMainWindow):
+class MPABS_GUI(QMainWindow):
     def __init__(self):
         self.home_page = HomePage()
         self.home_page.show()
 
 app = QApplication(sys.argv)
-win = MainActivity()
+win = MPABS_GUI()
 sys.exit(app.exec_())
